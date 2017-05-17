345561adc13b81b3f45a33466a9ba47041aa3f62
playrix <web.playrix@yandex.ru>
2017-05-17 22:09:46 +0000

    Task 2

---
+++ module_01/task_02.rb
@@ -0,0 +1,11 @@
+# Площадь треугольника
+
+puts "Введите основание треугольника :"
+base = gets.chomp.to_i
+
+puts "Введите его высоту :"
+height = gets.chomp.to_i
+
+area_triangle = 0.5 * base * height
+
+puts "Площадь треугольника равна : #{area_triangle}"
